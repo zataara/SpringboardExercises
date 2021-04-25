8b251bb604a8d74d87e291c5b9c7084a60c87471
zataara <bryantmac@gmail.com>
2021-04-25 14:21:24 -0700

    added test file for js exercises

---
+++ javascript/js-exercises/destructuring.js
@@ -0,0 +1,6 @@
+//What does the following code return/print?
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // ?
+console.log(yearNeptuneDiscovered); // ? 
