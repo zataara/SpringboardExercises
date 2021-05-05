9769ae2aefc7365413e8493b992766d431064af9
zataara <bryantmac@gmail.com>
2021-05-04 18:20:43 -0700

    added api get functionality

--- other/ajax-giphy-party/app.js
+++ other/ajax-giphy-party/app.js
@@ -1,1 +1,41 @@
-console.log("Let's get this party started!");
+const $gifs = $("#gifs");
+const $searchInput = $("#search");
+
+/* use ajax result to add a gif */
+
+function addGif(res) {
+  let numResults = res.data.length;
+  if (numResults) {
+    let randomIdx = Math.floor(Math.random() * numResults);
+    let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+    let $newGif = $("<img>", {
+      src: res.data[randomIdx].images.original.url,
+      class: "w-100"
+    });
+    $newCol.append($newGif);
+    $gifs.append($newCol);
+  }
+}
+
+/* handle form submission: clear search box & make ajax call */
+
+$("form").on("submit", async function(evt) {
+  evt.preventDefault();
+
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    }
+  });
+  addGif(response.data);
+});
+
+/* remove gif */
+
+$("#remove").on("click", function() {
+  $gifs.empty();
+});
