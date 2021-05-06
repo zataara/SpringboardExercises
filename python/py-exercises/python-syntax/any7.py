c926b2f62d655f73d497406df00a3e568fb0126f
zataara <bryantmac@gmail.com>
2021-05-06 13:09:53 -0700

    completed exercise

--- python/py-exercises/python-syntax/any7.py
+++ python/py-exercises/python-syntax/any7.py
@@ -3,6 +3,13 @@ def any7(nums):
 
     # YOUR CODE HERE 
 
+    for num in nums:
+        if num == 7:
+            return True
+        
+    return False
+
+
 print("should be true", any7([1, 2, 7, 4, 5]))
 print("should be false", any7([1, 2, 4, 5]))
 
