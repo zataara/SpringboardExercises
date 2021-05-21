24447c7cc03afc832e60561397edb5b6352e9f3e
zataara <bryantmac@gmail.com>
2021-05-21 14:11:37 -0700

    completed exercise

--- python/py-exercises/data-structures/sum_pairs.py
+++ python/py-exercises/data-structures/sum_pairs.py
@@ -21,3 +21,5 @@ def sum_pairs(nums, goal):
         >>> sum_pairs([11, 20, 4, 2, 1, 5], 100)
         ()
     """
+
+    
--- python/py-exercises/data-structures/vowel_count.py
+++ python/py-exercises/data-structures/vowel_count.py
@@ -7,3 +7,9 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+
+    count = {}
+    
+ 
+
+
