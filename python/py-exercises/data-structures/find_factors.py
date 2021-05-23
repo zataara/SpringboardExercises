59eea9e0c41c4922814f3eda4c419aef60fac03c
zataara <bryantmac@gmail.com>
2021-05-22 21:33:14 -0700

    completed exercise

--- python/py-exercises/data-structures/find_factors.py
+++ python/py-exercises/data-structures/find_factors.py
@@ -13,3 +13,10 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+
+    factors = []
+
+    for i in range(1,num +1):
+        if num % i == 0:
+            factors.append(i)
+    return factors
--- python/py-exercises/data-structures/repeat.py
+++ python/py-exercises/data-structures/repeat.py
@@ -18,3 +18,9 @@ def repeat(phrase, num):
         >>> repeat('abc', 'nope') is None
         True
     """
+
+    if not isinstance(num, int) or num < 0:
+        return None
+
+    return phrase * num
+    
