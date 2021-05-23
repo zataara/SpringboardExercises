50b6e1feff7018eef2fc6df26da2e3721eec939a
zataara <bryantmac@gmail.com>
2021-05-23 16:36:55 -0700

    completed exercise

--- python/py-exercises/data-structures/valid_parentheses.py
+++ python/py-exercises/data-structures/valid_parentheses.py
@@ -22,3 +22,15 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+    count = 0
+
+    for p in parens:
+        if p == '(':
+            count += 1
+        elif p == ')':
+            count -= 1
+
+        if count < 0:
+            return False
+
+    return count == 0
