1e0b5ef06202a1dbc81fd484a90aa9950d413498
zataara <bryantmac@gmail.com>
2021-05-24 23:23:20 -0700

    created subclass that filters words with different file setups

--- python/py-exercises/python-oo-practice/wordfinder.py
+++ python/py-exercises/python-oo-practice/wordfinder.py
@@ -1,5 +1,37 @@
+import random
+
 """Word Finder: finds random words from a dictionary."""
 
 
 class WordFinder:
-    ...
+    """Machine to find random words
+
+    
+
+    """
+
+    def __init__(self, filepath):
+        reference_file = open(filepath)
+        self.words = self.parse(reference_file)
+        print(f'{len(self.words)} words read')
+
+
+    def parse(self, reference_file):
+        return[l.strip() for l in reference_file]
+    
+    def random(self):
+        return random.choice(self.words)
+        
+
+    
+
+class SpecialWordFinder(WordFinder):
+
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def parse(self, reference_file):
+        return[l.strip() for l in reference file if l.strip() and not l.startswith('#')]
+
+
+
