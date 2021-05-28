15189b2a628840187999bc553d538b3f654c9a42
zataara <bryantmac@gmail.com>
2021-05-27 22:07:35 -0700

    flask work

--- python/py-exercises/flask/app.py
+++ python/py-exercises/flask/app.py
@@ -3,3 +3,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def home_page():
+    html = '''
+        <html>
+            <body>
+                <h1>Hello There!</h1>
+            </body>
+        </html>
+            '''
+    return html
+
+
+@app.route('/hello')
+def say_hello():
+    html = '''
+        <html>
+            <body>
+                <h1>Hello There!</h1>
+            </body>
+        </html>
+            '''
+    return html
+
+
+
+
+@app.route('/goodbye')
+def say_goodbye():
+    return 'see you later'
