9c48ec9e3b6682e115dee381d87efe15debcd8a4
zataara <bryantmac@gmail.com>
2021-08-31 01:25:52 -0700

    completed part 2

--- javascript/js-promises/Part 2/app.js
+++ javascript/js-promises/Part 2/app.js
@@ -10,11 +10,37 @@ $(function() {
         let deckID = data.deck_id;
         let firstCard = data.cards[0]
         console.log(firstCard)
-        return $.getJSON(`${baseURL}${deckID}/draw`)
+        return $.getJSON(`${baseURL}/${deckID}/draw`)
     }).then(data => {
         let secondCard = data.cards[0]
         console.log(secondCard)
     })
 
 
+    let deckId = null;
+    let $btn = $('button');
+    let $cardArea = $('#card-area');
+
+    $.getJSON(`${baseURL}/new/shuffle/`).then(data => {
+        deckId = data.deck_id;
+        $btn.show();
+    });
+
+    $btn.on('click', function() {
+        $.getJSON(`${baseURL}/${deckId}/draw/`).then(data => {
+        let cardSrc = data.cards[0].image;
+        let angle = Math.random() * 90 - 45;
+        let randomX = Math.random() * 40 - 20;
+        let randomY = Math.random() * 40 - 20;
+        $cardArea.append(
+            $('<img>', {
+            src: cardSrc,
+            css: {
+                transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+            }
+            })
+        );
+        if (data.remaining === 0) $btn.remove();
+        });
+    });
 });
