7ac950d85f0dc3848d92ff0aff9d9a833543f5c3
zataara <bryantmac@gmail.com>
2021-08-31 00:43:10 -0700

    completed question 2

--- javascript/js-promises/Part 2/app.js
+++ javascript/js-promises/Part 2/app.js
@@ -1,1 +1,20 @@
 
+
+
+$(function() {
+    let baseURL = 'http://deckofcardsapi.com/api/deck/' ;
+
+    $.getJSON(`${baseURL}new/draw/?count=1`).then(data => {
+        let {suit,value} = data.cards[0];
+        console.log(`Your card is the ${value.toLowerCase()} of ${suit.toLowerCase()}`)
+        let deckID = data.deck_id;
+        let firstCard = data.cards[0]
+        console.log(firstCard)
+        return $.getJSON(`${baseURL}${deckID}/draw`)
+    }).then(data => {
+        let secondCard = data.cards[0]
+        console.log(secondCard)
+    })
+
+
+});
