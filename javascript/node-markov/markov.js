349673454fd994ad5edc0d91e812357702d21d1e
zataara <bryantmac@gmail.com>
2021-09-02 18:01:15 -0700

    complete markov machine

--- javascript/node-markov/markov.js
+++ javascript/node-markov/markov.js
@@ -17,13 +17,37 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
-  }
+    let chains = new Map();
 
+    for (let i = 0; i < this.words.length; i ++) {
+      let word  = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (chains.has(word)) chains.get(word).push(nextWord);
+      else chains.set(word, [nextWOrd]);
+    }
+    this.chains = chains;
+  }
 
   /** return random text from chains */
+  static choice(ar) {
+    return ar[Math.floor(Math.random() * ar.length)]
+  }
+
 
   makeText(numWords = 100) {
-    // TODO
+    let keys = Array.from(this.chains.keys());
+    let key  = MarkovMachine.choice(keys);
+    let out = [];
+
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+    return out.join('')
   }
+}
+
+module.exports = {
+  MarkovMachine,
 }
