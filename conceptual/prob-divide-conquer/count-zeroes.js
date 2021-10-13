1ebc48544033369845af57ce487ebf239efd40c8
zataara <bryantmac@gmail.com>
2021-10-12 19:10:04 -0700

    completed, all test passing

--- conceptual/prob-divide-conquer/count-zeroes.js
+++ conceptual/prob-divide-conquer/count-zeroes.js
@@ -1,5 +1,30 @@
-function countZeroes() {
+function countZeroes(arr) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
 
+    // look for edge cases of all 0s or all 1s
+    if(arr[leftIdx] === 0) {
+            return arr.length
+        } else if(arr[rightIdx] === 1){
+            return 0;
+        }
+
+    // divide array in half
+    while(leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx)/2);
+        let middleVal = arr[middleIdx];
+        
+        // look for a 0 with a 1 before it
+        if(middleVal === 0 && arr[middleIdx - 1] === 1) {
+            return arr.length - middleIdx;
+        } else if(middleVal === 1 ) {
+            leftIdx = middleIdx + 1;
+        } else if(middleVal === 0) {
+            rightIdx = middleIdx - 1;
+        } else {
+            return arr.length
+        }
+    }
 }
 
 module.exports = countZeroes
