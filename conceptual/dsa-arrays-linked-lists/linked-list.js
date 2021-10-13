41afe7146012b91b0646dea438c536590c93508f
zataara <bryantmac@gmail.com>
2021-10-13 15:18:11 -0700

    adding new methods for linked lists

--- conceptual/dsa-arrays-linked-lists/linked-list.js
+++ conceptual/dsa-arrays-linked-lists/linked-list.js
@@ -21,31 +21,65 @@ class LinkedList {
   /** push(val): add new value to end of list. */
 
   push(val) {
+    let newNode = new Node(val);
 
+    // if there is no head to the list yet, make newNode the head of the list.
+    if(this.head === null) {
+      this.head = newNode;
+    }
+
+    // if there is a tail to the list, place newNode after the tail.
+    if(this.tail !== null) {
+      this.tail.next = newNode;
+    }
+
+    //change the new tail to be the newNode we just pushed
+    this.tail = newNode;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
-
+    let newNode = new Node(val);
+    this.head = newNode;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
-
+    let poppedTail = this.tail;
+    let current = this.head;
+    while(current.next !== null) {
+      if (current.next === poppedTail) {
+        current.next === null;
+      }
+    }
+    return poppedTail;
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
+    let headToRemove = this.head;
+    headToRemove.next = this.head;
+    
+    return headToRemove;
 
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-
+    let count = 0;
+    let current = this.head;
+    while(current !== null) {
+      if(current = idx) {
+        return count
+      }
+      current = current.next;
+      count ++
+    }
+    return new Error('Invalid Index');
   }
 
   /** setAt(idx, val): set val at idx to val */
