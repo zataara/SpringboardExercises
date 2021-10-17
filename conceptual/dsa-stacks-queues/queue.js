44e910ec1d820a90771c75c3acb9f9483af4f39e
zataara <bryantmac@gmail.com>
2021-10-17 13:48:02 -0700

    all tests passing

--- conceptual/dsa-stacks-queues/queue.js
+++ conceptual/dsa-stacks-queues/queue.js
@@ -20,26 +20,45 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    let node = new Node(val)
 
+    if(!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+    this.last.next = node;
+    this.last = node;
+    }
+    this.size ++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if(!this.first) {
+      throw new Error("Can't dequeue from an empty queue.");
+    }
+    let toReturn = this.first;
+    if(this.first == this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size --;
 
+    return toReturn.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
