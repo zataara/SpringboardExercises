452afa5fdb80c9cb6dab84e23986aca5158e7b7b
zataara <bryantmac@gmail.com>
2021-10-17 14:31:42 -0700

    all tests passing

--- conceptual/dsa-stacks-queues/stack.js
+++ conceptual/dsa-stacks-queues/stack.js
@@ -20,26 +20,48 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
+    let node = new Node(val);
 
+    if(!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      let tmp = this.first;
+      this.first = node;
+      this.first.next = tmp
+    }
+    this.size ++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if(!this.first) {
+      throw new Error("Can't pop from an empty stack");
+    }
+    let temp = this.first.val;
+
+    if(this.first == this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.size --;
+    return temp;
 
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
