28841891bef042a01e740e260012502ee3457dff
zataara <bryantmac@gmail.com>
2021-10-28 08:37:52 -0700

    added new traversal functions

--- data-structures/dsa-bsts/binary-search-tree.js
+++ data-structures/dsa-bsts/binary-search-tree.js
@@ -110,28 +110,77 @@ class BinarySearchTree {
    * Return an array of visited nodes. */
 
   dfsPreOrder() {
+    let data = [];
+    let current = this.root;
 
+    function traverse(node) {
+      data.push(node.val); // visit
+      node.left && traverse(node.left); // go left if there's a left
+      node.right && traverse(node.right); // go right if there's a right
+    }
+
+    traverse(current);
+    return data;
   }
 
   /** dfsInOrder(): Traverse the array using in-order DFS.
    * Return an array of visited nodes. */
 
   dfsInOrder() {
+    let data = [];
+    let current = this.root;
 
+    function traverse(node) {
+      
+      node.left && traverse(node.left); // go left if there's a left
+      data.push(node.val); // visit
+      node.right && traverse(node.right); // go right if there's a right
+    }
+
+    traverse(current);
+    return data;
   }
 
   /** dfsPostOrder(): Traverse the array using post-order DFS.
    * Return an array of visited nodes. */
 
   dfsPostOrder() {
+    let data = [];
+    let current = this.root;
 
+    function traverse(node) {
+      
+      node.left && traverse(node.left); // go left if there's a left
+      
+      node.right && traverse(node.right); // go right if there's a right
+      data.push(node.val); // visit
+    }
+
+    traverse(current);
+    return data;
   }
 
   /** bfs(): Traverse the array using BFS.
    * Return an array of visited nodes. */
 
   bfs() {
+    let node = this.root;
+    let queue = [];
+    let data = [];
 
+    queue.push(node);
+
+    while(queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+      if(node.left) {
+        queue.poush(node.left);
+      }
+      if(node.right) {
+        queue.push(node.right);
+      }
+    }
+    return data;
   }
 
   /** Further Study!
@@ -139,22 +188,107 @@ class BinarySearchTree {
    * Returns the removed node. */
 
   remove(val) {
+    let nodeToRemove = this.root;
+    let parent;
 
+    while (nodeToRemove.val !== val) {
+      parent = nodeToRemove;
+      if (val < nodeToRemove.val) {
+        nodeToRemove = nodeToRemove.left;
+      } else {
+        nodeToRemove = nodeToRemove.right;
+      }
+    }
+
+    if (nodeToRemove !== this.root) {
+      if (nodeToRemove.left === null && nodeToRemove.right === null) {
+        if (parent.left === nodeToRemove) {
+          parent.left = null;
+        } else {
+          parent.right = null;
+        }
+      } else if (nodeToRemove.left !== null && nodeToRemove.right !== null) {
+        let rightParent = nodeToRemove;
+        let right = nodeToRemove.right;
+        if (right.left === null) {
+          right.left = nodeToRemove.left;
+          if (parent.left === nodeToRemove) {
+            parent.left = right;
+          } else {
+            parent.right = right;
+          }
+        } else {
+          while (right.left !== null) {
+            rightParent = right;
+            right = right.left;
+          }
+          if (parent.left === nodeToRemove) {
+            parent.left.val = right.val;
+          } else {
+            parent.right.val = right.val;
+          }
+          if (right.right !== null) {
+            rightParent.left = right.right;
+          } else {
+            rightParent.left = null;
+          }
+        }
+      } else {
+        if (parent.left === nodeToRemove) {
+          if (nodeToRemove.right === null) {
+            parent.left = nodeToRemove.left;
+          } else {
+            parent.left = nodeToRemove.right;
+          }
+        } else {
+          if (nodeToRemove.right === null) {
+            parent.right = nodeToRemove.left;
+          } else {
+            parent.right = nodeToRemove.right;
+          }
+        }
+      }
+    }
+    return nodeToRemove;
   }
 
   /** Further Study!
    * isBalanced(): Returns true if the BST is balanced, false otherwise. */
 
-  isBalanced() {
+  isBalanced(current = this.root) {
+    if (current === null) return;
+    return maxDepth(current) - minDepth(current) <= 1;
 
+    function minDepth(current) {
+      if (current === null) return 0;
+      return 1 + Math.min(minDepth(current.left), minDepth(current.right));
+    }
+
+    function maxDepth(current) {
+      if (current === null) return 0;
+      return 1 + Math.max(maxDepth(current.left), maxDepth(current.right));
+    }
   }
 
   /** Further Study!
    * findSecondHighest(): Find the second highest value in the BST, if it exists.
    * Otherwise return undefined. */
 
-  findSecondHighest() {
-    
+  findSecondHighest(current = this.root) {
+    // if the tree is too small, return
+    if (!this.root || (!this.root.left && !this.root.right)) return;
+
+    while (current) {
+      // Current is largest and has a left subtree and 2nd largest is the largest in that subtree
+      if (current.left && !current.right) {
+        return this.findSecondHighest(current.left);
+      }
+      // Current is parent of largest and largest has no children so current is 2nd largest
+      if (current.right && (!current.right.left && !current.right.right)) {
+        return current.val;
+      }
+      current = current.right;
+    }
   }
 }
 
