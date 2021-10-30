675c44f3d15051d5f5fd454ca5376cf17d65b406
zataara <bryantmac@gmail.com>
2021-10-30 13:38:36 -0700

    all tests passing

--- data-structures/dsa-graphs/graph.js
+++ data-structures/dsa-graphs/graph.js
@@ -36,13 +36,88 @@ class Graph {
   }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    return this.nodes.delete(vertex)
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const visited = new Set();
+    const result = [];
+
+    function traverse(vertex) {
+      if(!vertex) {
+        return null
+      }
+      visited.add(vertex)
+      result.push(vertex.value);
+
+      vertex.adjacent.forEach(neighbor => {
+        if(!visited.has(neighbor)) {
+          return traverse(neighbor)
+        }
+      });
+    }
+    traverse(start)
+    return result
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    const queue = [start];
+    const result = [];
+    const visited = new Set();
+    let currentVertex;
+
+    visited.add(start);
+
+    while(queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex.value);
+
+      currentVertex.adjacent.forEach(neighbor => {
+        if(!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+        }
+      })
+    }
+    return result;
+  }
+
+  shortestPath(start, end) {
+    if (start === end) {
+      return [start.value];
+    }
+
+    var queue = [start];
+    let visited = new Set();
+    let predecessors = {};
+    let path = [];
+
+    while (queue.length) {
+      let currentVertex = queue.shift();
+
+      if (currentVertex === end) {
+        let stop = predecessors[end.value];
+        while (stop) {
+          path.push(stop);
+          stop = predecessors[stop];
+        }
+        path.unshift(start.value);
+        path.reverse();
+        return path;
+      }
+
+      visited.add(currentVertex);
+      for (let vertex of currentVertex.adjacent) {
+        if (!visited.has(vertex)) {
+          predecessors[vertex.value] = currentVertex.value;
+          queue.push(vertex);
+        }
+      }
+    }
+  }
 }
 
 module.exports = {Graph, Node}
