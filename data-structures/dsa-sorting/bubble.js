31bcb9bbe87c4d76cce3584f8316923ee027b6db
zataara <bryantmac@gmail.com>
2021-11-01 13:57:26 -0700

    added function for bubble sorting

--- data-structures/dsa-sorting/bubble.js
+++ data-structures/dsa-sorting/bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let swapped = false
+        for (let j = 0; j < arr.length - i; j++) {
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
+        if(!swapped) break;
+    }
+    return arr;
+}
 
 module.exports = bubbleSort;
